f148697429f3d42ea8bfd4330528f3cbd9c397a4
PPMGKKApplProgTeacher <ppmg_kk_appl_prog_teacher@proton.me>
2024-12-09 09:12:44 +0200

    Create Investigator.cs

---
+++ Investigator.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+using System.Text;
+
+public class Investigator
+{
+    public void Investigate(string className, string[] fieldNames)
+    {
+        // TODO
+    }
+}
