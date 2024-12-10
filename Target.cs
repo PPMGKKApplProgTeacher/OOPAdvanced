0c079bf2c73a966d07ae30b12032893cdf583d32
PPMGKKApplProgTeacher <ppmg_kk_appl_prog_teacher@proton.me>
2024-12-10 07:44:38 +0200

    Update Target.cs

--- Target.cs
+++ Target.cs
@@ -27,3 +27,63 @@ public class Target
         this.PrivateBalance += amount;
     }
 }
+
+
+Exercise 1: Steal Field Info
+
+Task: Implement a method that retrieves and prints the value of PublicInfo and PrivateInfo.
+Solution
+
+Spy spy = new Spy();
+Console.WriteLine(spy.StealFieldInfo("Target", "PublicInfo", "PrivateInfo"));
+
+Expected Output:
+
+Class under investigation: Target
+PublicInfo = PublicData
+PrivateInfo = SecretData
+
+Exercise 2: Analyze Access Modifiers
+
+Task: Verify the access modifiers of the Target class fields and methods.
+Solution
+
+Console.WriteLine(spy.AnalyzeAccessModifiers("Target"));
+
+Expected Output:
+
+PublicInfo must be private!
+get_PublicId have to be public!
+set_PublicId have to be private!
+get_PrivateBalance have to be public!
+set_PrivateBalance have to be private!
+
+Exercise 3: Reveal Private Methods
+
+Task: List all private methods in the Target class.
+Solution
+
+Console.WriteLine(spy.RevealPrivateMethods("Target"));
+
+Expected Output:
+
+All Private Methods of Class: Target
+Base Class: Object
+GetPrivateInfo
+UpdateBalance
+Finalize
+MemberwiseClone
+
+Exercise 4: Collect Getters and Setters
+
+Task: Identify getters and setters in the Target class, along with their return and parameter types.
+Solution
+
+Console.WriteLine(spy.CollectGettersAndSetters("Target"));
+
+Expected Output:
+
+get_PublicId will return System.Int32
+get_PrivateBalance will return System.Double
+set_PublicId will set field of System.Int32
+set_PrivateBalance will set field of System.Double
